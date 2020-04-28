cadfc8299520398f0c1443361cf82b1b25665415
chalumeau <chalumeauk@gmail.com>
2020-04-28 15:24:59 +0200

    creation groupe etape 2

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -5,34 +5,8 @@ namespace App\Controller;
 use App\Model\GroupManager;
 use App\Model\CityManager;
 
-
-class GroupController extends AbstractController{
-    
-    public function index(){
-        $groupManager = new GroupManager();
-        $group= $groupManager->selectAll();
-
-        return $this->twig->render('');
-    }
-
-    /*public function show($id){
-        $musicianManager = new MusicianManager();
-        $musician = $musicianManager->selectOneById($id);
-
-        return $this->twig->render();
-    }
-
-    public function edit($id){
-        $musicianManager = new MusicianManager();
-        $musician = $musicianManager->selectOneById($id);
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-            $musician ['']= $_POST[''];
-            $musicianManager->update($musician);
-        }
-        return $this->twig->render();
-
-    }*/
+class GroupController extends AbstractController
+{
     /**
      * affiche et traite le formulaire d'inscription d'un groupe.
      */
@@ -45,41 +19,37 @@ class GroupController extends AbstractController{
         $cityManager = new CityManager();
         $cities = $cityManager->selectAll();
         // Si la requête est POST, je traite le formulaire
-        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             //$groupManager = new GroupManager;
             //on vérifie que tous les champs sont présent.
-            if(\filter_has_var(INPUT_POST,"name") && \filter_has_var(INPUT_POST,"email")  ){
+            if (\filter_has_var(INPUT_POST, "name") && \filter_has_var(INPUT_POST, "email")) {
                 //je valide le champ name
                 $size = \strlen($_POST["name"]);
-                if($size < 1 || $size > 255) {
+                if ($size < 1 || $size > 255) {
                     $error["name"] = "Le nom du groupe doit faire entre 1 et 255 caractères";
-                }
-                else {
+                } else {
                     $group["name"] = $_POST["name"];
                 }
                 //je valide le champ email
-                if (filter_input(INPUT_POST, "email",FILTER_VALIDATE_EMAIL) === false) {
+                if (filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL) === false) {
                     $error["email"] = "l'adresse mail n'est pas valide";
-                }
-                else {
+                } else {
                     $group["email"] = $_POST["email"];
                 }
                 //y'a t'il un champ qui n'est pas valide???
-                if(!empty($error)){
-                    return $this->twig->render("Group/add.html.twig", ["errors" => $error, "group" => $group, "cities" => $cities]);
+                if (!empty($error)) {
+                    return $this->twig->render(
+                        "Group/add.html.twig",
+                        ["errors" => $error, "group" => $group, "cities" => $cities]
+                    );
                 }
                 //tous les champs sont valide
+            } else {
+                // Le cas où il manque des données.
             }
-            // Le cas où il manque des données.
-            else {
-                
-            }   
-            
-        }
-        //La requete n'est pas une requete POST, j'affiche le formulaire vide.
-        else {
-            return $this->twig->render('Group/add.html.twig',["cities" => $cities]);
-
+        } else {
+            //La requete n'est pas une requete POST, j'affiche le formulaire vide.
+            return $this->twig->render('Group/add.html.twig', ["cities" => $cities]);
         }
     }
 }
--- src/Model/GroupManager.php
+++ src/Model/GroupManager.php
@@ -2,7 +2,8 @@
 
 namespace App\Model;
 
-class GroupManager extends AbstractManager{
+class GroupManager extends AbstractManager
+{
 
     const  TABLE = 'group';
 
@@ -11,8 +12,12 @@ class GroupManager extends AbstractManager{
         parent::__construct(self::TABLE);
     }
 
-    public function insert($musician){
-        $statement=$this->pdo->prepare("INSERT INTO". self::TABLE . "(`name`,`email`,`city`,desciption`, `password`) VALUES (:name, :email, :city, :description, :password)");
+    public function insert($musician)
+    {
+        $statement=$this->pdo->prepare(
+            "INSERT INTO". self::TABLE .
+            "(`name`,`email`,`city`,desciption`, `password`) VALUES (:name, :email, :city, :description, :password)"
+        );
         $statement->bindValue('name', $musician, \PDO::PARAM_STR);
         $statement->bindValue('email', $musician, \PDO::PARAM_STR);
         $statement->bindValue('city', $musician, \PDO::PARAM_STR);
@@ -20,14 +25,10 @@ class GroupManager extends AbstractManager{
         $statement->bindValue('password', $musician, \PDO::PARAM_STR);
         $statement->execute();
     }
-    public function delete($id){
+    public function delete($id)
+    {
         $statement = $this->pdo->prepare("DELETE FROM" . self::TABLE . "WHERE id=:id");
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
     }
-
-    /*public function update($musician){
-        $statement = $this->pdo->prepare("UPDATE" . self::TABLE . "SET `ps")
-    }*/
-
 }
