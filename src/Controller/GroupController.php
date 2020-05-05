ffd6be25140440942f959706b7b4f5be8fa9447d
chalumeau <chalumeauk@gmail.com>
2020-05-05 10:40:42 +0200

    modif controllergroupe etape2

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -23,8 +23,8 @@ class GroupController extends AbstractController
             //$groupManager = new GroupManager;
             //on vérifie que tous les champs sont présent.
             if (\filter_has_var(INPUT_POST, "name") &&
-                \filter_has_var(INPUT_POST, "email") &&
-                \filter_has_var(INPUT_POST, "description")) {
+                \filter_has_var(INPUT_POST, "description") &&
+                \filter_has_var(INPUT_POST, "city")) {
                 //je valide le champ name
                 $size = \strlen($_POST["name"]);
                 if ($size < 1 || $size > 255) {
@@ -32,13 +32,20 @@ class GroupController extends AbstractController
                 } else {
                     $group["name"] = $_POST["name"];
                 }
-                //je valide le champ email
-                if (filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL) === false) {
-                    $error["email"] = "l'adresse mail n'est pas valide";
-                } else {
-                    $group["email"] = $_POST["email"];
-                }
+                
                 //je valide le champ texte
+                if ($size < 1 || $size > 800) {
+                    $error["description"] = "La descritpion de votre groupe est obligatoire";
+                } else {
+                    $group["description"] = $_POST["description"];
+                }
+               
+                //je valide le champ city
+                if (empty($_POST["city"])) {
+                    $error["city"] = "Le choix de la ville est obligatoire";
+                } else {
+                    $group["city"] = $_POST["city"];
+                }
                 //y'a t'il un champ qui n'est pas valide???
                 if (!empty($error)) {
                     return $this->twig->render(
@@ -53,6 +60,45 @@ class GroupController extends AbstractController
         } else {
             //La requete n'est pas une requete POST, j'affiche le formulaire vide.
             return $this->twig->render('Group/add.html.twig', ["cities" => $cities]);
+        }
+    }
+    public function addConect()
+    {
+        //On initialise le tableau des erreurs.
+        $error = [];
+        //on initialise le tableau groupe.
+        $group = [];
+        // Si la requête est POST, je traite le formulaire
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (\filter_has_var(INPUT_POST, "email") &&
+               \filter_has_var(INPUT_POST, "password")) {
+                //je valide le champ email
+                if (filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL) === false) {
+                    $error["email"] = "l'adresse mail n'est pas valide";
+                } else {
+                    $group["email"] = $_POST["email"];
+                }
+                 //je valide le champ password
+                 $size = \strlen($_POST["name"]);
+                if ($size < 1 || $size > 255) {
+                    $error["password"] = "Le mot de passe doit faire entre 1 et 255 caractères";
+                } else {
+                    $group["password"] = $_POST["password"];
+                }
+                //y'a til un champ qui n'est pas valide?
+                if (!empty($error)) {
+                    return $this->twig->render(
+                        "Group/add.html.twig",
+                        ["errors" => $error, "group" => $group]
+                    );
+                }
+            } else {
+                //tous les champs sont valide.
+                return $this->twig->render('Group/profil.html.twig');
+            }
+        } else {
+             //La requete n'est pas une requete POST, j'affiche le formulaire vide.
+             return $this->twig->render('Group/add.html.twig');
         }
     }
 }
