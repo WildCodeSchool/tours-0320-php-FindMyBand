5ad85f135e8bde81197dfde7cb40998c362377c1
sarah <sarah-lambert@live.fr>
2020-05-12 12:01:37 +0200

    page contact +navbar+footer+css pour harmonisation du style

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by Romain Clair
+ * 24/04/2020
+ */
+
+namespace App\Controller;
+
+
+
+/**
+ * Class InstrumentController
+ *
+ */
+class ContactController extends AbstractController
+{
+
+    /**
+     * Display instruments listing
+     *
+     * This page and controller will probably disapear but we'll keep it for testing purpose
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        
+
+        return $this->twig->render('Contact/index.html.twig');
+    }
+}
