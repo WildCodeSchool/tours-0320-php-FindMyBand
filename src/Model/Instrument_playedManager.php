9863ecd7eea4f992569f2ea051a06c27a434cb03
ahmad <ahmadsafar257@gmail.com>
2020-05-13 15:55:56 +0200

    manipuler la table instrument_played

---
+++ src/Model/Instrument_playedManager.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by VScode.
+ * User: Ahmad
+ * Date: 05/05/2020
+ * Time: 09:34
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+use App\Model\Mastery_levelManager;
+use App\Model\InstrumentManager;
+use App\Model\MusicianManager;
+use App\Model\MusicianForm;
+
+/**
+ *
+ */
+class Instrument_playedManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'instrument_played';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+
+    /**
+     * @param int $instrument_played
+     * @return int
+     */
+    public function insert(int $masteryId,int $instrumentId,int $musicianId) : int
+    {
+        $statement = $this->pdo->prepare(
+        "INSERT INTO " .
+         self::TABLE . " (`mastery_levels_id`,`instrument_id`,`musician_id`)
+          VALUES (:mastery_levels_id,:instrument_id,:musician_id)");
+        $statement->bindValue('mastery_levels_id', $masteryId, \PDO::PARAM_INT);
+        $statement->bindValue('instrument_id', $instrumentId, \PDO::PARAM_INT);
+        $statement->bindValue('musician_id', $musicianId, \PDO::PARAM_INT);
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
+}
