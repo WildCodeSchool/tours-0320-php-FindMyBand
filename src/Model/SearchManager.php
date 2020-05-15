dbbe1efcebce8ed24bf023ee34e451573fcfe561
ahmad <ahmadsafar257@gmail.com>
2020-05-15 15:34:22 +0200

    dérniaire commit

--- src/Model/SearchManager.php
+++ src/Model/SearchManager.php
@@ -36,10 +36,9 @@ class SearchManager extends AbstractManager
         // prepared request
         $statement=$this->pdo->prepare("INSERT INTO" .
                                         self::TABLE . "(`instrument_id`,`group_id`,`mastery_levels_id`) 
-                                        VALUES (:instrument_id, :group_id, :mastery_levels_id)"
-                                      );
+                                        VALUES (:instrument_id, :group_id, :mastery_levels_id)");
         $statement->bindValue('instrument_id', $instrumentId, \PDO::PARAM_INT);
-        $statement->bindValue('group_id',$groupId, \PDO::PARAM_INT);
+        $statement->bindValue('group_id', $groupId, \PDO::PARAM_INT);
         $statement->bindValue('mastery_levels_id', $masteryId, \PDO::PARAM_INT);
         if ($statement->execute()) {
             return (int)$this->pdo->lastInsertId();
@@ -48,9 +47,9 @@ class SearchManager extends AbstractManager
     public function allannonce(): array
     {
         $statement = $this->pdo->prepare("SELECT search.id as id , instrument_id, group_id, mastery_levels_id
-        FROM  $this->table JOIN instrument as i ON i.id= instrument_id JOIN mastery_levels as m ON m.id=mastery_levels_id");
+        FROM  $this->table
+        JOIN instrument as i ON i.id= instrument_id JOIN mastery_levels as m ON m.id=mastery_levels_id");
         $statement->execute();
         return $statement->fetchAll();
     }
-
 }
